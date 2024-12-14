a2ea61c93c8267c25962dfaa66a51457133cd953
leah <leah@email.com>
2024-12-14 11:54:37 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Banner from './Banner'
+import Topsellers from './Topsellers'
+import Recommened from './Recommened'
+import News from './News'
+
+const Home = () => {
+  return (
+    <>
+    <Banner/>
+    <Topsellers/>
+    <Recommened/>
+    <News/>
+        </>
+  )
+}
+
+export default Home
